9a296c67e5681991d799ca131815ef3a89143bf4
Lukanin Dmitriy <d.lukanin@qiwi.com>
2017-02-03 12:46:22 +0300

    routes change

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import * as config from 'config';
 
-import {router} from './app/routes';
+import {router} from './app/routes/user';
 import {App} from "innots";
 
 const app = new App(config, router);
--- app/routes/user.ts
+++ app/routes/user.ts
@@ -1,7 +1,7 @@
 import * as Router from 'koa-router';
 import * as config from 'config';
-import {Users as UsersController} from './controllers/users';
-import {Auth as AuthController} from './controllers/auth';
+import {Users as UsersController} from '../controllers/users';
+import {Auth as AuthController} from '../controllers/auth';
 
 const router = new Router();
 const users = new UsersController();
@@ -66,19 +66,6 @@ router
      *
      * @apiSuccess {Boolean} result результат удаления.
      */
-    .post(userBaseRoute + 'remove', users.deleteItem)
-    /**
-     * @api {post} /public/login
-     * @apiName auth
-     * @apiGroup Auth
-     *
-     * @apiDescription Попытка залогиниться.
-     * @apiParam {String} email Email пользователя.
-     * @apiParam {String} password Пароль.
-     *
-     * @apiSuccess {String} result token.
-     */
-    //TODO own router
-    .post('/public/login', auth.login);
+    .post(userBaseRoute + 'remove', users.deleteItem);
 
 export {router};
